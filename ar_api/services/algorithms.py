a9656c47f3e52a427214a646247d9431e68ac9f6
romakot321 <play62342@gmail.com>
2023-03-26 15:30:09 +0800

    Fix alg

--- ar_api/services/algorithms.py
+++ ar_api/services/algorithms.py
@@ -1,22 +1,19 @@
 from math import sqrt
-from ar_api import schemas
+#from ar_api import schemas
 
 
 def get_path(
     start_coord: tuple[float, float], 
     stop_coord: tuple[float, float]
-) -> list[schemas.path.Coords]:
-
+#) -> list[schemas.path.Coords]:
+):
     start_coord = [round(start_coord[0]), round(start_coord[1])]
-    end_coord = [round(stop_coord[0]), round(stop_coord[1])]
+    stop_coord = [round(stop_coord[0]), round(stop_coord[1])]
     diff_x, diff_y = 1, 1
 
     path = []
     while start_coord != stop_coord:
-        diff_x = start_coord[0] - stop_coord[0]
-        diff_y = start_coord[1] - stop_coord[1]
-
-
+        print(start_coord, stop_coord)
         if start_coord[0] < stop_coord[0]:
             start_coord[0] += 1
         if start_coord[0] > stop_coord[0]:
@@ -26,7 +23,8 @@ def get_path(
         if start_coord[1] > stop_coord[1]:
             start_coord[1] -= 1
 
-        path.append(schemas.path.Coords(x=start_coord[0], y=start_coord[1]))
+        path.append(tuple(start_coord))
+        #path.append(schemas.path.Coords(x=start_coord[0], y=start_coord[1]))
 
     return path
 
